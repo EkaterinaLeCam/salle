5c7b41671ce069041ed0ad68e05370f500731d1f
katia <katia.lecam@gmail.com>
2023-05-16 10:19:28 +0200

    salle

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\SalleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SalleController extends AbstractController
 {
-    #[Route('/salle', name: 'app_salle')]
-    public function index(): Response
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('salle/index.html.twig', [
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/salle/{id}', name: 'app_salle', methods: ['GET', 'POST'])]
+    public function show($id): Response
+    {
+        // Affiche la salle demandée dans le template dédie
+        $oneSalle = $this->entityManager->getRepository(Salle::class)->findOneBy(['id' =>$id]);
+
+        return $this->render('salle/salle.html.twig', [
+            //recupere la note demandée par son id
             'controller_name' => 'SalleController',
+            'oneSalle' => $oneSalle
         ]);
     }
 }
--- src/Controller/SallesController.php
+++ src/Controller/SallesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SalleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class SallesController extends AbstractController
 {
     #[Route('/salles', name: 'app_salles')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('salles/index.html.twig', [
+        $salle =$entityManager->getRepository(Salle::class)->findAll();
+
+        return $this->render('salles/salles.html.twig', [
             'controller_name' => 'SallesController',
+            'salle' =>$salle
+            
         ]);
     }
 }
